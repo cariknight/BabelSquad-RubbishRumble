86b4f43ed53e1dea723967f9e93edef415ba1667
Rica Caballero <2024rpcaballero@live.mcl.edu.ph>
2026-07-14 01:05:31 +0800

    initialized DatabaseService.cs

---
+++ RubbishRumble/Services/DatabaseService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RubbishRumble.Models;
+using SQLite;
+
+namespace RubbishRumble.Services
+{
+    public class DatabaseService
+    {
+        private SQLiteAsyncConnection database;
+
+        public async Task InitAsync()
+        {
+
+        }
+
+        public async Task<List<Player>> GetPlayersAsync()
+        {
+
+        }
+
+        public async Task SavePlayerAsync(Player player)
+        {
+
+        }
+
+        public async Task SaveGameSessionAsync(GameSession session)
+        {
+
+        }
+    }
+}
