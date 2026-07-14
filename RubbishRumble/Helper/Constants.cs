6b9b98b1df40095acbaa3f3f39e568692b5af798
Rica Caballero <2024rpcaballero@live.mcl.edu.ph>
2026-07-14 16:05:13 +0800

    [GameService.cs] base game mechanics done

--- RubbishRumble/Helper/Constants.cs
+++ RubbishRumble/Helper/Constants.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RubbishRumble.Helper
+﻿namespace RubbishRumble.Helper
 {
-    internal class Constants
+    public class Constants
     {
+        public const int STARTING_LIVES = 3;
+
+        // Spawn Settings
+        public const double STARTING_SPAWN_INTERVAL = 3.0;
+        public const double MIN_SPAWN_INTERVAL = 0.8;
+
+        // Trash Speed settings
+        public const double STARTING_TRASH_SPEED = 1.0;
+        public const double MAX_TRASH_SPEED = 5.0;
+
+        // Rarity settings
+        public const double STARTING_RARITY_MULTIPLIER = 1.0;
+        public const double MAX_RARITY_MULTIPLIER = 3.0;
+
+        // Difficulty settings
+        public const int DIFFICULTY_INCREASE_EVERY = 20;
+
+        public const double SPAWN_INTERVAL_DECREASE = 0.1;
+        public const double TRASH_SPEED_INCREASE = 0.15;
+        public const double RARITY_MULTIPLIER_INCREASE = 0.1;
     }
 }
--- RubbishRumble/Services/GameService.cs
+++ RubbishRumble/Services/GameService.cs
@@ -1,55 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
+using RubbishRumble.Models;
+using RubbishRumble.Helper;
+
 
 
 namespace RubbishRumble.Services
 {
     public class GameService
     {
-        //Start game
-        //End game
-        //Increase score
-        //Lose life
-        //Spawn trash
+        //Start game - done
+        //End game -done
+        //Increase score -done
+        //Lose life - done
+        //Spawn trash - 
         //Activate power-ups
         //Increase difficulty
+
+        //1. Load trashitems.json.
+        //2. Load rarity.json.
+        //3. Pick a rarity using Chance.
+        //4. Find all trash items with that rarity.
+        //5. Randomly choose one of those items.
+        //6. Give the player the Points from that rarity.
+        //7. Calculate coins.
 
         public int Score { get; private set; }
         public int Lives { get; private set; }
         public int Coins { get; private set; }
-        public bool IsGGameOver => Lives <= 0;
+        public int DifficultyLevel { get; private set; }
+        public double SpawnInterval { get; private set; }
+        public double TrashSpeed { get; private set; }
+        public double RarityMultiplier { get; set; } 
+
+        // Game
+        public bool IsGameOver => Lives <= 0;
 
         public void StartGame()
         {
             Score = 0;
-            Coins = 0;
-            Lives = 3;
+            Lives = Constants.STARTING_LIVES;
+
+            DifficultyLevel = 1;
+
+            SpawnInterval = Constants.STARTING_SPAWN_INTERVAL;
+
+            TrashSpeed = Constants.STARTING_TRASH_SPEED;
+
+            RarityMultiplier = Constants.STARTING_RARITY_MULTIPLIER;
         }
 
-        public void AddScore(int amount)
+        public void AddScore(int points)
         {
-            Score += amount;
-        }
-
-        public void AddCoins(int amount)
-        {
-            Coins += amount;
+            Score += points;
+            IncreaseDifficulty();
         }
 
         public void LoseLife()
         {
             Lives--;
+
             if (Lives <= 0)
             {
                 EndGame();
             }
         }
 
-        public void Endame()
+        public int CalculateCoins()
         {
+            // you can edit here how much score is converted into coins (i.e., Score/2 120 score = 60 coins)
+            return Score;
+        }
+        public GameSession EndGame()
+        {
+            return new GameSession
+            {
+                // Total coins not added yet
+                FinalScore = Score,
+                CoinsEarned = CalculateCoins(),
+                PlayedAt = DateTime.Now
+            };
+        }
+        public void IncreaseDifficulty()
+        {
+            DifficultyLevel++;
+
+            SpawnInterval = Math.Max(Constants.MIN_SPAWN_INTERVAL, SpawnInterval - 0.1);
+            TrashSpeed = Math.Min(Constants.MAX_TRASH_SPEED, TrashSpeed + Constants.SPAWN_INTERVAL_DECREASE);
+            RarityMultiplier = Math.Min(Constants.MAX_RARITY_MULTIPLIER, RarityMultiplier + Constants.RARITY_MULTIPLIER_INCREASE);
+        }
+
+        public void CollectTrash(TrashItem trash, Rarity rarity)
+        {
+            int scoreEarned = rarity.PointValue;
+            int coinsEarned = rarity.PointValue * 2;
+            Score += scoreEarned;
 
         }
     }
