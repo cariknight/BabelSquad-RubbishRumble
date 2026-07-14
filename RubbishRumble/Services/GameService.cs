78055c29ad1edc71027b3f3bf093c2fff97275ba
Rica Caballero <2024rpcaballero@live.mcl.edu.ph>
2026-07-14 08:51:14 +0800

    initialized GameService.cs

--- RubbishRumble/Services/GameService.cs
+++ RubbishRumble/Services/GameService.cs
@@ -3,10 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Android.Telephony.CarrierConfigManager;
+using static Microsoft.Maui.LifecycleEvents.AndroidLifecycle;
 
 namespace RubbishRumble.Services
 {
-    internal class GameService
+    public class GameService
     {
+        //Start game
+        //End game
+        //Increase score
+        //Lose life
+        //Spawn trash
+        //Activate power-ups
+        //Increase difficulty
+
+        public int Score { get; private set; }
+        public int Lives { get; private set; }
+        public int Coins { get; private set; }
+        public bool IsGGameOver => Lives <= 0;
+
+        public void StartGame()
+        {
+            Score = 0;
+            Coins = 0;
+            Lives = 3;
+        }
+
+        public void AddScore(int amount)
+        {
+            Score += amount;
+        }
+
+        public void AddCoins(int amount)
+        {
+            Coins += amount;
+        }
+
+        public void LoseLife()
+        {
+            Lives--;
+            if (Lives <= 0)
+            {
+                EndGame();
+            }
+        }
+
+        public void Endame()
+        {
+
+        }
     }
 }
