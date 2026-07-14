3b7136c19c4946f17043a01b81a47a2c972d850f
czayle <2024cztfernandez@live.mcl.edu.ph>
2026-07-15 04:25:25 +0800

    EDIT: made main menu page work (temporarily made gameover ui as opening)

--- RubbishRumble/App.xaml.cs
+++ RubbishRumble/App.xaml.cs
@@ -6,8 +6,7 @@
         {
             InitializeComponent();
 
-            //MainPage = new AppShell();
-            MainPage = new Views.GameOverPage();
+            MainPage = new AppShell();
         }
     }
 }
