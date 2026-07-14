b12178bc51be7111d3bc4ab98726c39e33cc33a0
Rica Caballero <2024rpcaballero@live.mcl.edu.ph>
2026-07-14 11:18:13 +0800

    Updated PowerUp.cs and PowerUpService.cs, Initialized powerup.json

--- RubbishRumble/Models/PowerUp.cs
+++ RubbishRumble/Models/PowerUp.cs
@@ -10,8 +10,11 @@ namespace RubbishRumble.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string EffectType { get; set; }
+        public string Description { get; set; }
         public int DurationSeconds { get; set; }
         public double ScoreMultiplier { get; set; }
+        public double SpeedMultiplier { gget; set; }
         public int Quantity { get; set; }
     }
 }
