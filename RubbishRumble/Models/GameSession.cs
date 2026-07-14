e43bb877f4ea7d56f9ec9316ca0bc2debe0ece60
Rica Caballero <2024rpcaballero@live.mcl.edu.ph>
2026-07-14 11:26:24 +0800

    Updated Inventory.cs and GameSession.cs

--- RubbishRumble/Models/GameSession.cs
+++ RubbishRumble/Models/GameSession.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace RubbishRumble.Models
 {
     public class GameSession
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public int PlayerId { get; set; }
         public int FinalScore { get; set; }
