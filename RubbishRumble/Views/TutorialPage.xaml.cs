507772e2529ecc3eecbbcf81846d5820f9f101d8
Rica Caballero <2024rpcaballero@live.mcl.edu.ph>
2026-07-22 19:36:03 +0800

    CHANGE: Switched to Net 9.0 build to fix Carousel view error on windows machine

--- RubbishRumble/Views/TutorialPage.xaml.cs
+++ RubbishRumble/Views/TutorialPage.xaml.cs
@@ -1,5 +1,3 @@
-using System;
-using Microsoft.Maui.Controls;
 using RubbishRumble.Services;
 
 namespace RubbishRumble.Views
@@ -13,76 +11,117 @@ namespace RubbishRumble.Views
             public string ImageUrl { get; set; } = string.Empty;
         }
 
-        private List<TutorialSlide> _slides;
+        private readonly List<TutorialSlide> _slides;
+        private bool _carouselInitialized;
+
         public TutorialPage()
         {
             InitializeComponent();
 
-            _slides = new List<TutorialSlide>
+            _slides =
+            [
+                new TutorialSlide
                 {
-                    new TutorialSlide
-                    {
-                        Title = "Swipe left or right\r\nto move items",
-                        Description = "Match trash with\r\nits correct bin!",
-                        ImageUrl = "tutorial_slide1.png"
-                    },
-                    new TutorialSlide
-                    {
-                        Title = "Blue bin for\r\nRecyclables",
-                        Description = "Green bin for\r\nBiodegradable",
-                        ImageUrl = "tutorial_slide2.png"
-                    },
-                    new TutorialSlide
-                    {
-                        Title = "Red bin for\r\nBiohazard",
-                        Description = "Gray bin for\r\nLandfill",
-                        ImageUrl = "tutorial_slide3.png"
-                    },
-                    new TutorialSlide
-                    {
-                        Title = "You start with 3 lives",
-                        Description = "Use power-ups wisely!",
-                        ImageUrl = "tutorial_slide4.png"
-                    },
-                    new TutorialSlide
-                    {
-                        Title = "Earn coins per item",
-                        Description = "Points depend on\r\ntrash rarity",
-                        ImageUrl = "tutorial_slide5.png"
-                    },
-                    new TutorialSlide
-                    {
-                        Title = "Common Trash =\r\nBasic Points",
-                        Description = "Worth 5 coins\r\nFound most often",
-                        ImageUrl = "tutorial_common.png"
-                    },
-                    new TutorialSlide
-                    {
-                        Title = "Uncommon Trash = Better Points",
-                        Description = "Worth 10 coins\r\nAppears sometimes",
-                        ImageUrl = "tutorial_uncommon.png"
-                    },
-                    new TutorialSlide
-                    {
-                        Title = "Rare Trash = Big Points",
-                        Description = "Worth 20 coins\r\nHard to find",
-                        ImageUrl = "tutorial_rare.png"
-                    },
-                    new TutorialSlide
-                    {
-                        Title = "Epic Trash = Jackpot!",
-                        Description = "Worth 50 coins\r\nExtremely rare",
-                        ImageUrl = "tutorial_epic.png"
-                    },
-                    new TutorialSlide
-                    {
-                        Title = "You're ready!",
-                        Description = "Let's keep the world clean together",
-                        ImageUrl = "tutorial_slide6.png"
-                    }
-                };
+                    Title = "Swipe left or right\r\nto move items",
+                    Description = "Match trash with\r\nits correct bin!",
+                    ImageUrl = "tutorial_slide1.png"
+                },
+                new TutorialSlide
+                {
+                    Title = "Blue bin for\r\nRecyclables",
+                    Description = "Green bin for\r\nBiodegradable",
+                    ImageUrl = "tutorial_slide2.png"
+                },
+                new TutorialSlide
+                {
+                    Title = "Red bin for\r\nBiohazard",
+                    Description = "Gray bin for\r\nLandfill",
+                    ImageUrl = "tutorial_slide3.png"
+                },
+                new TutorialSlide
+                {
+                    Title = "You start with 3 lives",
+                    Description = "Use power-ups wisely!",
+                    ImageUrl = "tutorial_slide4.png"
+                },
+                new TutorialSlide
+                {
+                    Title = "Earn coins per item",
+                    Description = "Points depend on\r\ntrash rarity",
+                    ImageUrl = "tutorial_slide5.png"
+                },
+                new TutorialSlide
+                {
+                    Title = "Common Trash =\r\nBasic Points",
+                    Description = "Worth 5 coins\r\nFound most often",
+                    ImageUrl = "tutorial_common.png"
+                },
+                new TutorialSlide
+                {
+                    Title = "Uncommon Trash = Better Points",
+                    Description = "Worth 10 coins\r\nAppears sometimes",
+                    ImageUrl = "tutorial_uncommon.png"
+                },
+                new TutorialSlide
+                {
+                    Title = "Rare Trash = Big Points",
+                    Description = "Worth 20 coins\r\nHard to find",
+                    ImageUrl = "tutorial_rare.png"
+                },
+                new TutorialSlide
+                {
+                    Title = "Epic Trash = Jackpot!",
+                    Description = "Worth 50 coins\r\nExtremely rare",
+                    ImageUrl = "tutorial_epic.png"
+                },
+                new TutorialSlide
+                {
+                    Title = "You're ready!",
+                    Description = "Let's keep the world clean together",
+                    ImageUrl = "tutorial_slide6.png"
+                }
+            ];
+
+            TutorialCarousel.PositionChanged += OnCarouselPositionChanged;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (_carouselInitialized)
+                return;
+
+            _carouselInitialized = true;
+            InitializeCarousel();
+        }
+
+        private void InitializeCarousel()
+        {
+#if WINDOWS
+            // Defer binding until after Shell navigation completes on WinUI.
+            Dispatcher.Dispatch(() =>
+            {
+                TutorialCarousel.ItemsSource = _slides;
+                TutorialCarousel.IndicatorView = TutorialIndicator;
+                UpdateNextButton(_slides.Count - 1);
+            });
+#else
             TutorialCarousel.ItemsSource = _slides;
             TutorialCarousel.IndicatorView = TutorialIndicator;
+            UpdateNextButton(_slides.Count - 1);
+#endif
+        }
+
+        private void OnCarouselPositionChanged(object? sender, PositionChangedEventArgs e)
+        {
+            UpdateNextButton(_slides.Count - 1);
+        }
+
+        private void UpdateNextButton(int lastPosition)
+        {
+            bool isLastSlide = TutorialCarousel.Position >= lastPosition;
+            NextImageButton.Source = isLastSlide ? "play.png" : "next.png";
         }
 
         private async void OnNextButtonClicked(object sender, EventArgs e)
@@ -93,8 +132,7 @@ namespace RubbishRumble.Views
 
             if (currentPosition < lastPosition)
             {
-                int nextPosition = currentPosition + 1;
-                TutorialCarousel.CurrentItem = _slides[nextPosition];
+                TutorialCarousel.Position = currentPosition + 1;
             }
             else
             {
